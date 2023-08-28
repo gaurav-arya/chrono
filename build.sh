c9fb29597d01a31ed0243d9cbdae67fb55f04850
Gaurav Arya <gauravarya272@gmail.com>
2023-08-28 14:43:06 -0400

    Add enzyme into cmake

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 cd build_chrono
 cmake .. -D EIGEN3_INCLUDE_DIR=/home/gridsan/aryag/local_installs/eigen-3.4.0 -D CMAKE_INSTALL_PREFIX=/home/gridsan/aryag/local_installs
+make -j 48
+make install
 cd ..
